6d4faa339a66f304edc39312aa91265f1e82c0fe
Konrad Szpak <konradszpak67@gmail.com>
2021-05-11 10:16:39 +0100

    Add project files.

---
+++ KonradSzpak_S00197298_FinalExam2/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KonradSzpak_S00197298_FinalExam2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ KonradSzpak_S00197298_FinalExam2/Game.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.Entity;
+
+namespace KonradSzpak_S00197298_FinalExam2
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public int MetacriticScore { get; set; }
+        public string Description { get; set; }
+        public string Platform { get; set; }
+        public decimal Price { get; set; }
+        public string GameImage { get; set; }
+
+        public Game()
+        {
+        }
+        public Game(string name, int metacriticScore, string description, string platform, decimal price, string gameImage)
+        {
+            Name = name;
+            MetacriticScore = metacriticScore;
+            Description = description;
+            Platform = platform;
+            Price = price;
+            GameImage = gameImage;
+        }
+        public override string ToString()
+        {
+            return Name;
+
+        }
+        public void DecreasePrice(double decrease)
+        {
+            Price *= (decimal)(1 - decrease);
+        }
+
+        public class GameData : DbContext
+        {
+            public GameData() : base("GameInfo") { }
+            public DbSet<Game> Games { get; set; }
+        }
+    }
+}
+
