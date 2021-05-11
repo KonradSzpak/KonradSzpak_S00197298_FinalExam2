859a31979f49e0084d1be5d26df0649c0a1abe03
Konrad Szpak <konradszpak67@gmail.com>
2021-05-11 10:30:32 +0100

    more changes

--- KonradSzpak_S00197298_FinalExam2/Game.cs
+++ KonradSzpak_S00197298_FinalExam2/Game.cs
@@ -5,7 +5,7 @@ namespace KonradSzpak_S00197298_FinalExam2
     public class Game
     {
         public string Name { get; set; }
-        public int MetacriticScore { get; set; }
+        public int CriticScore { get; set; }
         public string Description { get; set; }
         public string Platform { get; set; }
         public decimal Price { get; set; }
@@ -17,7 +17,7 @@ namespace KonradSzpak_S00197298_FinalExam2
         public Game(string name, int metacriticScore, string description, string platform, decimal price, string gameImage)
         {
             Name = name;
-            MetacriticScore = metacriticScore;
+            CriticScore = metacriticScore;
             Description = description;
             Platform = platform;
             Price = price;
@@ -33,11 +33,12 @@ namespace KonradSzpak_S00197298_FinalExam2
             Price *= (decimal)(1 - decrease);
         }
 
-        public class GameData : DbContext
-        {
-            public GameData() : base("GameInfo") { }
-            public DbSet<Game> Games { get; set; }
-        }
+       
+    }
+    public class GameData : DbContext
+    {
+        public GameData() : base("GameInfo") { }
+        public DbSet<Game> Games { get; set; }
     }
 }
 
